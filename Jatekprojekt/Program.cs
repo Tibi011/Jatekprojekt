13522df3d3510b2debcc83d2d4a0b46bf7d43456
Tibi011 <megsztil@gmail.com>
2025-02-20 08:00:37 +0100

    a

--- Jatekprojekt/Program.cs
+++ Jatekprojekt/Program.cs
@@ -8,6 +8,19 @@ namespace Jatekprojekt
 {
     internal class Program
     {
+        public class Character
+        {
+            public string Name { get; set; }
+            public string Class { get; set; }
+            public int Strength { get; set; }
+            public int Defense { get; set; }
+            public int Dexterity { get; set; }
+            public int Constitution { get; set; }
+            public int Intelligence { get; set; }
+            public int Wisdom { get; set; }
+            public int Charisma { get; set; }
+        }
+
         static void Main(string[] args)
         {
             string DnD = @" 
@@ -31,41 +44,40 @@ namespace Jatekprojekt
                                                                  | . \| | |  __/ |_) |  __/\__ \ | |  | |___ ___) | |___   | |
                                                                  |_|\_\_|_|\___| .__/ \___||___/ | |  |_____|____/ \____|  | |
                                                                                |_|                \_\                     /_/ ";
+            string Karakter = @"
+                                      _  _                             _           _              _                         _     _                          _   _ 
+                                     | \| |  ___  __ __  ___   ___  __| |    ___  | |    __ _    | |__  __ _   _ _   __ _  | |__ | |_   ___   _ _   ___   __| | (_)
+                                     | .` | / -_) \ V / / -_) |_ / / _` |   / -_) | |   / _` |   | / / / _` | | '_| / _` | | / / |  _| / -_) | '_| / -_) / _` |  _ 
+                                     |_|\_| \___|  \_/  \___| /__| \__,_|   \___| |_|   \__,_|   |_\_\ \__,_| |_|   \__,_| |_\_\  \__| \___| |_|   \___| \__,_| (_)";
             Console.WriteLine(DnD);
             Console.WriteLine(Jatek);
             Console.WriteLine(Kilepes);
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-            Console.Write("Nevezd el a karaktered: ");
-            string karnev=Console.ReadLine();
-            Console.WriteLine($"Üdvözöllek {karnev}!");
-        osztalyvalaszt:
-            Console.WriteLine("Válaszd ki a classed: (Warrior | Archer | Mage | Jester)");
-            string war1 = @"
+            ConsoleKey billentyu;
+                billentyu = Console.ReadKey(true).Key;
+       
+                if(billentyu != ConsoleKey.Escape) 
+            {
+                Console.Clear();
+                Console.WriteLine(Karakter + " ");
+                Console.Write("\t\t\t\t\t\t\t\t\t\t\t\t");
+                Character karakter = new Character();
+                karakter.Name = Console.ReadLine();
+                Console.Clear();
+            osztalyvalaszt:
+                Console.WriteLine("Válaszd ki a classed: (Warrior | Archer | Mage | Jester)");
+                string war1 = @"
                             1. Warrior (Harcos)
                             A harcos egy erőteljes közelharci karakter, aki a fizikai erejére és a védekezésre összpontosít. Képes nagy mennyiségű sérülést okozni, és jól bírja a csatákat.
                             Strength (Erő): A harcos fizikai ereje, amely meghatározza a támadások erejét és a súlyos páncéllal való mozgás képességét.
                             Defense (Védelem): A harcos képessége, hogy elkerülje vagy csökkentse a támadások hatását. Minél magasabb, annál jobban képes ellenállni a sérüléseknek.
                             Dexterity (Ügyesség): A harcos gyorsasága és mozgékonysága. Ez befolyásolja a támadások sebességét, valamint a csapdák és a támadások elkerülésének esélyét.";
-            string ar1 = @"
+                string ar1 = @"
                             2. Archer (Íjász)
                             Az íjász távolsági harcra specializálódott, és nagy hangsúlyt fektet a kitartásra, az erőre és a védekezésre. Ő egy pontos és gyors támadó, aki a harc távolabbi aspektusait uralja.
                             Strength (Erő): Az íjász fizikai ereje, amely befolyásolja a távolsági támadásokat (pl. nyílzápor) és a súlyosabb íjak használatát.
                             Defense (Védelem): Az íjász is képes elkerülni vagy csökkenteni a támadások hatását, de a hangsúly inkább a mozgékonyságon van, mint a páncél viselésén.
                             Constitution (Állóképesség): Az íjász állóképessége és kitartása a hosszú távú küzdelmekhez. Ez befolyásolja az életpontok számát és a sérülésekhez való ellenállást.";
-            string mag1 = @"
+                string mag1 = @"
                             3. Mage (Mágus)
                             A mágus a varázslásra összpontosít, és a tudása és bölcsessége révén irányítja az elemeket.
                             A mágus a legkevésbé védett a közelharci támadások ellen, de rendkívül erős varázslatokkal rendelkezik.
@@ -77,7 +89,7 @@ namespace Jatekprojekt
                             valamint a varázslatok hatékonyságát és azok tanulásának gyorsaságát.
                             Wisdom(Bölcsesség): A mágus bölcsessége befolyásolja a varázslatok előrejelzését, az érzékelési képességeit és azt, hogy hogyan reagál a mágikus hatásokra. 
                             Segít a varázslatok pontosabb végrehajtásában és a mágikus világ megértésében.";
-            string jest1 = @"
+                string jest1 = @"
                             4.Jester(Bolond):
                             Charisma (Karizma):
                             A jester legerősebb tulajdonsága, mivel a karizma segíti őt a szórakoztatásban, manipulációban és társas kapcsolatokban.
@@ -88,24 +100,55 @@ namespace Jatekprojekt
                             Defense (Védelem):
                             A jester nem rendelkezik komoly páncéllal, de az ügyessége és trükkjei segíthetnek abban, hogy elkerülje a támadásokat. A védekezés mérsékelten fontos számára.";
 
-            Console.WriteLine(war1);
-            Console.WriteLine(ar1);
-            Console.WriteLine(mag1);
-            Console.WriteLine(jest1);
-            string osztaly =Console.ReadLine();
-            if(osztaly.ToLower()== "warrior" || osztaly.ToLower()== "archer" || osztaly.ToLower()== "mage" || osztaly.ToLower()=="jester")
-            {
-                if(osztaly.ToLower()== "warrior")
+                Console.WriteLine(war1);
+                Console.WriteLine(ar1);
+                Console.WriteLine(mag1);
+                Console.WriteLine(jest1);
+                string osztaly = Console.ReadLine();
+                if (osztaly.ToLower() == "warrior" || osztaly.ToLower() == "archer" || osztaly.ToLower() == "mage" || osztaly.ToLower() == "jester")
                 {
-                    
+                    Console.Clear();
+                    if (osztaly.ToLower() == "warrior")
+                    {
+                        Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t\tWarrior kiválasztva.");
+                        karakter.Class = "Harcos";
+                        karakter.Strength = 16;  
+                        karakter.Defense = 14;  
+                        karakter.Dexterity = 12;
+                        Console.WriteLine("\n\t\t\t\t\t\t\t\t\t\t\t\tA karaktered létrejött!");
+                        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t\tNév: {karakter.Name}");
+                        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t\tOsztály: {karakter.Class}");
+                        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t\tStrength: {karakter.Strength}");
+                        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t\tDefense: {karakter.Defense}");
+                        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t\tDexterity: {karakter.Dexterity}");
+                    }
                 }
+
+                else
+                {
+                    Console.WriteLine("Helytelen karakter!");
+                    goto osztalyvalaszt;
+                }
+
+
+
             }
 
             else
             {
-                Console.WriteLine("Helytelen karakter!");
-                goto osztalyvalaszt;
+                Console.WriteLine("Kilépés a játékból.");
             }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
